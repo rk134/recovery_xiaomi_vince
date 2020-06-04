3e3c8497bd2b71b6cb0d596b6b33afed07f349a3
Captain Throwback <captainthrowback@hotmail.com>
2020-06-04 10:31:56 -0400

    vince: Build some blobs from source
    Some of these blobs are included in the build by default
    For the others, add as TARGET_RECOVERY_DEVICE_MODULES and
    use TW_RECOVERY_ADDITIONAL_RELINK_FILES to include in /sbin
    
    Change-Id: Ie32a76cd6297df194fff614d17f428008630965f
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,3 +123,21 @@ TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_EDL_MODE := false
 TW_INPUT_BLACKLIST := "hbtp_vm"
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libcap \
+    libicui18n \
+    libion \
+    libicuuc \
+    libpcrecpp \
+    libprocinfo \
+    libxml2
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libprocinfo.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
